80436e714feadf99a368583f9f8ef81dba025e26
oussamabng <oussama.bengoudifa.perso@gmail.com>
2022-03-25 23:09:14 +0100

    fix entity problem

---
+++ create_entity.py
@@ -0,0 +1,43 @@
+import os
+import argparse
+
+my_parser = argparse.ArgumentParser()
+my_parser.add_argument("--name",action="store",required=True)
+my_parser.add_argument("--entity",action="store",required=True)
+my_parser.add_argument("--attributes",action="store",required=True)
+init = my_parser.parse_args()
+
+path = os.path.join(os.getcwd(),init.name)
+data_types = ["String","Boolean","int","Long","double","Date"]
+attributes = init.attributes.split(",")
+filename = os.path.join(os.getcwd(),init.name,"src","main","java","com","example","{}".format(init.name),"entities","{}.java".format(init.entity))
+package = "package com.example.{}.entities;\n".format(init.name);
+persistence = "import javax.persistence.*;\n"
+date_import = "import java.util.Date;\n"
+
+
+def create():
+    with open(filename, 'w') as f:
+      f.write(package)
+      f.write(persistence)
+      f.write(date_import)
+
+      f.write("public class {}".format(init.entity))
+      f.write("{\n")
+      
+      for attribute in attributes:
+        name,type = attribute.split(":")
+        if (name == "id"):
+          f.write("@Id\n")
+          f.write("@GeneratedValue(strategy = GenerationType.IDENTITY)\n")
+        f.write("private {} {};\n".format(type,name))
+      
+      f.write("}")
+
+if __name__ == "__main__":
+  for attribute in attributes:
+        name,type = attribute.split(":")
+        if (type not in data_types):
+          print("this type: {} don't exist in JAVA".format(type))
+          exit(0)
+  create()
