a7adea5ce26f7827be07af91a3fc95bdbce7cb0f
oussamabng <oussama.bengoudifa.perso@gmail.com>
2022-04-02 20:42:29 +0100

    adding eureka options

--- name_of_the_project/src/main/java/com/example/name_of_the_project/repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.name_of_the_project.repositories;
-import com.example.name_of_the_project.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource
-public interface UserRepository extends JpaRepository<User,Long> {
-}
