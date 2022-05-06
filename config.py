0ace18b1f16d7382b8d6f9b1ff19caef8edfbd51
oussamabng <oussama.bengoudifa.perso@gmail.com>
2022-05-06 22:14:40 +0000

    adding feature mysql and h2 db config

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ my_parser = argparse.ArgumentParser()
 my_parser.add_argument("--port",action="store",required=True)
 my_parser.add_argument("--name",action="store",required=True)
 my_parser.add_argument("--dbName",action="store",required=True)
+my_parser.add_argument("--type_bdd",action="store",required=True)
 my_parser.add_argument("--eureka",action="store")
 
 init = my_parser.parse_args()
@@ -18,19 +19,27 @@ if (init.eureka and init.eureka == "true"):
 
 filename = os.path.join(os.getcwd(),init.name,"src","main","resources","application.properties")
 
-lines = ["server.port = {}".format(init.port),"spring.application.name={}".format(init.name),"spring.datasource.url=jdbc:h2:mem:{}".format(init.dbName),"spring.h2.console.enabled=true","spring.jpa.show-sql=true\n{}".format(eureka)]
+lines = ["server.port = {}\n".format(init.port),"spring.application.name={}\n".format(init.name),"spring.datasource.url=jdbc:h2:mem:{}\n".format(init.dbName),"spring.h2.console.enabled=true\n","spring.jpa.show-sql=true\n{}".format(eureka)]
+
+mysql = ["server.port = {}\n".format(init.port),"spring.application.name={}".format(init.name),"spring.datasource.url=jdbc:mysql://localhost:3306/{}\n".format(init.dbName),"spring.datasource.username=root\nspring.datasource.password=\n","spring.jpa.show-sql=true\n","spring.jpa.hibernate.ddl-auto=create-drop\n","spring.main.allow-bean-definition-overriding=true\n"]
 
 folders = ["entities","repositories","models","proxies","controllers"]
 
 
 def config_project():
   with open(filename, 'w') as f:
-    for line in lines:
-      f.write(line)
-      f.write("\n")
+    if (init.type_bdd == "h2"):
+      for line in lines:
+        f.write(line)
+        f.write("\n")
+    else:
+      for line in mysql:
+        f.write(line)
+        f.write("\n")
 
 def create_architecture():
   dir = os.path.join(os.getcwd(),init.name,"src","main","java","com","example","{}".format(init.name))
+  print(dir)
   if not os.path.isdir(dir):
     print("Folder {} do not exist".format(init.name))
     exit(0)
---
+++ tp1/src/main/java/com/example/tp1/Tp1Application.java
@@ -0,0 +1,13 @@
+package com.example.tp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp1Application.class, args);
+	}
+
+}
